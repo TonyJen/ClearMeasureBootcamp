76d1f7c13a26c136d83b9be29c1d32312905bf52
Tony Jen <tonyjen0905@gmail.com>
2017-10-19 07:48:29 -0400

    Fixed compilation error.

--- src/TestConsole/Program.cs
+++ src/TestConsole/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DataAccessEF;
+using ClearMeasure.Bootcamp.DataAccessEF.Mappings;
 
 namespace TestConsole
 {
