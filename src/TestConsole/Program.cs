d0fac64f44fc6827f2e29def7999f39cd08618a6
Tony Jen <tonyjen0905@gmail.com>
2017-10-19 12:12:20 -0400

    Added save functionality.

--- src/TestConsole/Program.cs
+++ src/TestConsole/Program.cs
@@ -17,13 +17,36 @@ namespace ClearMeasure.Bootcamp.TestCosole
         {
             
             _context = new DataContext();
+
+            ReadEmployee();
+            SaveEmployee();
+            ReadEmployee();
+        }
+
+        private static void SaveEmployee()
+        {
+            AutoMapper.Mapper.Initialize(cfg => cfg.CreateMap<DataAccessEF.Model.Employee, Core.Model.Employee>().ReverseMap());
+
+            Core.Model.Employee adminAssistant = new Core.Model.Employee("AssistantTemp", "Someone", "Else", "Email2");
+            adminAssistant.Id = Guid.NewGuid();
+            var EFEmployee = new DataAccessEF.Model.Employee();
+
+            Mapper.Map(adminAssistant, EFEmployee);
+            var _employee =_context.Set<DataAccessEF.Model.Employee>();
+            _employee.Add(EFEmployee);
+            _context.SaveChanges();
+
+        }
+
+        private static void ReadEmployee()
+        {
             AutoMapper.Mapper.Initialize(cfg => cfg.CreateMap<DataAccessEF.Model.Employee, Core.Model.Employee>());
             var Employee = _context.Employees;
-            foreach (var emp in Employee) {
+            foreach (var emp in Employee)
+            {
                 var model = Mapper.Map<DataAccessEF.Model.Employee, Core.Model.Employee>(emp);
                 Console.WriteLine(emp.FirstName.ToString());
             }
-
         }
     }
 }
